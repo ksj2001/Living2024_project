54c0f75a51330268fcd75a143856092526ba8a9a
cyanblayt <61958364+cyanblayt@users.noreply.github.com>
2024-01-17 15:01:29 +0900

    living_01day

---
+++ Livingntree/src/main/java/model/LivingDAO.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class LivingDAO {
+
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	Resultset rs = null;
+	
+	public void getConnect() {
+		try {
+			Context initctx = new InitialContext();
+			Context envctx = (Context) initctx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envctx.lookup("jdbc/pool");
+			con = ds.getConnection();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
